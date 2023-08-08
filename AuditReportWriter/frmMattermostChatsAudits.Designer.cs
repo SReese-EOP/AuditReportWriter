12ed2110a76bb5e5f3630d1a989d2ffae87783e2
Khart-EOP <kayla.m.hart@oa.eop.gov>
2023-08-08 12:46:09 -0400

    Created a new Mattermost chat audit page, the design window was not appearing

---
+++ AuditReportWriter/frmMattermostChatsAudits.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace AuditReportWriter
+{
+    partial class frmMattermostChatsAudits
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            bttnReturntoNav = new Button();
+            SuspendLayout();
+            // 
+            // bttnReturntoNav
+            // 
+            bttnReturntoNav.Location = new Point(469, 394);
+            bttnReturntoNav.Name = "bttnReturntoNav";
+            bttnReturntoNav.Size = new Size(193, 29);
+            bttnReturntoNav.TabIndex = 0;
+            bttnReturntoNav.Text = "Return to Navigation";
+            bttnReturntoNav.UseVisualStyleBackColor = true;
+            // 
+            // frmMattermostChatsAudits
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(bttnReturntoNav);
+            Name = "frmMattermostChatsAudits";
+            Text = "Mattermost Chat Audits";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button bttnReturntoNav;
+    }
+}
---
+++ AuditReportWriter/frmMattermostChatsAudits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AuditReportWriter
+{
+    public partial class frmMattermostChatsAudits : Form
+    {
+        public frmMattermostChatsAudits()
+        {
+            InitializeComponent();
+        }
+    }
+}
