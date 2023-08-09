59c4e627deb5a300522852c09ad4973e5dea6263
tp4orgood <temitope.o.oyeleye@oa.eop.gov>
2023-08-09 13:39:28 -0400

    Clear button work re-push

--- AuditReportWriter/Form1.cs
+++ AuditReportWriter/Form1.cs
@@ -1,10 +1,17 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Diagnostics;
+using System.Windows.Forms;
+
 namespace AuditReportWriter
 {
     public partial class frmEmailAuditReport : Form
     {
-        public frmEmailAuditReport()
+        private userInfo emailUser;
+
+        public frmEmailAuditReport(userInfo navuser)
         {
             InitializeComponent();
+            emailUser = navuser;
         }
 
         private void lblMessageToResult_Click(object sender, EventArgs e)
@@ -57,28 +64,45 @@ namespace AuditReportWriter
             {
                 //enable the groups for the audit reults
                 grpSenderResults.Enabled = true;
-                grpMessageReceivedTIme.Enabled = true;
+                grpReceivedTImeResults.Enabled = true;
                 grpTOResults.Enabled = true;
                 grpCCresults.Enabled = true;
+                grpBCCResults.Enabled = true;
+                grpSubjectResults.Enabled = true;
+                grpBodyResults.Enabled = true;
+                grpAttachementResult.Enabled = true;
                 //set the groups to blank for data entry
                 cboReceivedTimeResult.SelectedItem = null;
                 cboSenderResult.SelectedItem = null;
                 cboToAuditResult.SelectedItem = null;
                 cboCCAuditResult.SelectedItem = null;
+                cbobccAuditResult.SelectedItem = null;
+                cboSubjectValueResult.SelectedItem = null;
+                cboBodyValueResult.SelectedItem = null;
+                cboAttachementAuditResult.SelectedItem = null;
+
             }
             else
             {
                 //disable all the groups since the audit results are passing
                 grpSenderResults.Enabled = false;
-                grpMessageReceivedTIme.Enabled = false;
+                grpReceivedTImeResults.Enabled = false;
                 grpTOResults.Enabled = false;
                 grpCCresults.Enabled = false;
+                grpBCCResults.Enabled = false;
+                grpSubjectResults.Enabled = false;
+                grpBodyResults.Enabled = false;
+                grpAttachementResult.Enabled = false;
 
                 //mark all of the audit results as passing
                 cboReceivedTimeResult.SelectedItem = "PASS";
                 cboSenderResult.SelectedItem = "PASS";
                 cboToAuditResult.SelectedItem = "PASS";
                 cboCCAuditResult.SelectedItem = "PASS";
+                cbobccAuditResult.SelectedItem = "PASS";
+                cboSubjectValueResult.SelectedItem = "PASS";
+                cboBodyValueResult.SelectedItem = "PASS";
+                cboAttachementAuditResult.SelectedItem = "PASS";
             }
         }
 
@@ -98,7 +122,47 @@ namespace AuditReportWriter
 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
-            lblCCAuditResult.Text = "Hello World!";
+            //init a new email audit report object
+            EmailAuditReport emailAuditReport = new EmailAuditReport();
+
+            //init a new controller to write the audit report
+            AuditReportController auditReportController = new AuditReportController();
+
+            //add the audit report data
+            emailAuditReport.AuditDateTime = dtAuditDateTime.Value;
+            emailAuditReport.Mailbox = txtJournaledMailbox.Text;
+            emailAuditReport.MessageBody = cboBodyValueResult.SelectedItem.ToString();
+            emailAuditReport.MessageBodyValueExchange = txtBodyExchangeResult.Text;
+            emailAuditReport.MessageBodyValueOBS = txtBodyOBSResult.Text;
+            emailAuditReport.MessageSubject = cboSubjectValueResult.SelectedItem.ToString();
+            emailAuditReport.MessageSubjectValueExchange = txtSubjectExchangeResult.Text;
+            emailAuditReport.MessageSubjectValueOBS = txtSubjectOBSResult.Text;
+            emailAuditReport.MessageTo = cboToAuditResult.SelectedItem.ToString();
+            emailAuditReport.MessageTOValueExchange = txtExchToResult.Text;
+            emailAuditReport.MessageTOValueOBS = txtOBSToResult.Text;
+            emailAuditReport.MessageSender = cboSenderResult.SelectedItem.ToString();
+            emailAuditReport.MessageSenderValueExchange = txtExchSenderResult.Text;
+            emailAuditReport.MessageSenderValueOBS = txtOBSSenderResult.Text;
+            emailAuditReport.MessageReceivedTime = cboReceivedTimeResult.SelectedItem.ToString();
+            emailAuditReport.MessageReceivedTimeValueExchange = dtExchMessageRecievedTime.Value;
+            emailAuditReport.MessageReceivedTimeValueOBS = dtOBSMessageReceivedTime.Value;
+            emailAuditReport.MessageCC = cboCCAuditResult.SelectedItem.ToString();
+            emailAuditReport.MessageCCValueExchange = txtCCExchResult.Text;
+            emailAuditReport.MessageCCValueOBS = txtCCobsresult.Text;
+            emailAuditReport.MessageBCC = cbobccAuditResult.SelectedItem.ToString();
+            emailAuditReport.MessageBCCValueExchange = txtbccExchangeResult.Text;
+            emailAuditReport.MessageBCCValueOBS = txtbccOBSResult.Text;
+            emailAuditReport.MessageAttachments = cboAttachementAuditResult.SelectedItem.ToString();
+            emailAuditReport.MessageAttachmentsValueExchange = txtAttachmentExchangeResult.Text;
+            emailAuditReport.MessageAttachmentsValueOBS = txtAttachmentOBSResult.Text;
+            emailAuditReport.MessageId = txtMessageID.Text;
+            emailAuditReport.JournalEmailId = txtJounalEmailId.Text;
+            emailAuditReport.MessageAuditResult = cboOverallAuditResult.SelectedItem.ToString();
+            emailAuditReport.ObsObject = txtOBSObject.Text;
+
+            //execute the audit report write
+            auditReportController.WriteEmailAuditReport(emailAuditReport, emailUser.Server, emailUser.Database, emailUser.UserName, emailUser.Password);
+            MessageBox.Show("Email audit was successfully submitted under Audit ID: " + emailAuditReport.AuditId);
         }
 
         private void dtAuditDateTime_ValueChanged(object sender, EventArgs e)
@@ -153,6 +217,87 @@ namespace AuditReportWriter
         }
 
         private void cbobccAuditResult_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void txtExchSenderResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bttnReturnNav_Click(object sender, EventArgs e)
+        {
+            frmAuditNavigator frmAuditNavigator = new frmAuditNavigator(emailUser);
+            frmAuditNavigator.Show();
+            this.Hide();
+        }
+
+        private void cboSubjectValueResult_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboSubjectValueResult.SelectedItem != null && cboSubjectValueResult.SelectedItem == "FAIL")
+            {
+                txtSubjectExchangeResult.Enabled = true;
+                txtSubjectOBSResult.Enabled = true;
+            }
+            else
+            {
+                txtSubjectExchangeResult.Enabled = false;
+                txtSubjectOBSResult.Enabled = false;
+
+            }
+        }
+
+        private void cbobccAuditResult_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (cbobccAuditResult.SelectedItem != null && cbobccAuditResult.SelectedItem == "FAIL")
+            {
+                txtbccExchangeResult.Enabled = true;
+                txtbccOBSResult.Enabled = true;
+            }
+            else
+            {
+                txtbccExchangeResult.Enabled = false;
+                txtbccOBSResult.Enabled = false;
+            }
+        }
+
+        private void cboBodyValueResult_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboBodyValueResult.SelectedItem != null && cboBodyValueResult.SelectedItem == "FAIL")
+            {
+                txtBodyExchangeResult.Enabled = true;
+                txtBodyOBSResult.Enabled = true;
+            }
+            else
+            {
+                txtBodyExchangeResult.Enabled = false;
+                txtBodyOBSResult.Enabled = false;
+            }
+        }
+
+        private void cboAttachementAuditResult_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboAttachementAuditResult.SelectedItem != null && cboAttachementAuditResult.SelectedItem == "FAIL")
+            {
+                txtAttachmentExchangeResult.Enabled = true;
+                txtAttachmentOBSResult.Enabled = true;
+            }
+            else
+            {
+                txtAttachmentExchangeResult.Enabled = false;
+                txtAttachmentOBSResult.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtMessageID.Text = null;
+        }
+
+        private void txtMessageID_TextChanged(object sender, EventArgs e)
         {
 
         }
