5de90e776a936daae065e73038c427c2d100f84b
Khart-EOP <kayla.m.hart@oa.eop.gov>
2023-09-21 07:52:08 -0400

    commented out the portions of code that had no references

--- AuditReportWriter/frmAuditNavigator.cs
+++ AuditReportWriter/frmAuditNavigator.cs
@@ -22,41 +22,41 @@ namespace AuditReportWriter
             navUser = user;
         }
 
-        private void label1_Click(object sender, EventArgs e)
+/*        private void label1_Click(object sender, EventArgs e)
         {
 
-        }
-
+        }*/
+/*
         private void bttnExit_Click(object sender, EventArgs e)
         {
             Application.Exit();
-        }
+        }*/
 
         private void frmAuditNavigator_Load(object sender, EventArgs e)
         {
 
         }
-
+/*
         private void bttnEmailAudit_Click_1(object sender, EventArgs e)
         {
             frmEmailAuditReport frmEmailAuditReport = new frmEmailAuditReport(navUser);
             frmEmailAuditReport.Show();
             this.Hide();
-        }
+        }*/
 
-        private void bttnChatAudit_Click(object sender, EventArgs e)
+/*        private void bttnChatAudit_Click(object sender, EventArgs e)
         {
             frmMattermostChatsAudits frmMattermostChatsAudits = new frmMattermostChatsAudits(navUser);
             frmMattermostChatsAudits.Show();
             this.Hide();
-        }
-
+        }*/
+/*
         private void label1_Click_1(object sender, EventArgs e)
         {
 
-        }
+        }*/
 
-        private void cboSourceApplication_SelectedIndexChanged(object sender, EventArgs e)
+/*        private void cboSourceApplication_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (cboSourceApplication.SelectedItem.ToString() != null && cboSourceApplication.SelectedItem.ToString() == "Exchange")
             {
@@ -74,12 +74,12 @@ namespace AuditReportWriter
                 cboTypeofAudit.Enabled = true;
                 //cboTypeofAudit.Items.Add("Other Audit");
             }
-        }
+        }*/
 
-        private void button1_Click(object sender, EventArgs e)
+/*        private void button1_Click(object sender, EventArgs e)
         {
-            /*            cboSourceApplication.Items.Clear();
-                        cboTypeofAudit.Items.Clear();*/
+            *//*            cboSourceApplication.Items.Clear();
+                        cboTypeofAudit.Items.Clear();*//*
 
             if (cboTypeofAudit.SelectedItem.ToString() != null && cboTypeofAudit.SelectedItem.ToString() == "Chat Audit")
             {
@@ -103,17 +103,17 @@ namespace AuditReportWriter
                 //nothing
             }
 
-        }
+        }*/
 
-        private void cboTypeofAudit_SelectedIndexChanged(object sender, EventArgs e)
+/*        private void cboTypeofAudit_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (cboTypeofAudit != null)
             {
                 btnGo.Enabled = true;
                 bttnChanges.Enabled = true;
             }
-        }
-
+        }*/
+/*
         private void bttnChanges_Click(object sender, EventArgs e)
         {
             if (cboTypeofAudit.SelectedItem.ToString() != null && cboTypeofAudit.SelectedItem.ToString() == "Chat Audit")
@@ -122,11 +122,6 @@ namespace AuditReportWriter
                 frmMattermostChatCorrections.Show();
                 this.Close();
             }
-            else
-            {
-
-            }
-
 
             if (cboTypeofAudit.SelectedItem.ToString() != null && cboTypeofAudit.SelectedItem.ToString() == "Email Audit")
             {
@@ -135,7 +130,7 @@ namespace AuditReportWriter
                 this.Close();
 
             }
-        }
+        }*/
         private void lblSelectAudit_Click(object sender, EventArgs e)
         {
 
@@ -143,22 +138,74 @@ namespace AuditReportWriter
 
         private void cboSourceApplication_SelectedIndexChanged_1(object sender, EventArgs e)
         {
-
+            if (cboSourceApplication.SelectedItem.ToString() != null && cboSourceApplication.SelectedItem.ToString() == "Exchange")
+            {
+                cboTypeofAudit.Items.Clear();
+                cboTypeofAudit.Text = string.Empty;
+                cboTypeofAudit.Items.Add("Email Audit");
+                cboTypeofAudit.Enabled = true;
+                //cboTypeofAudit.Items.Add("Calendar Audit");
+            }
+            if (cboSourceApplication.SelectedItem.ToString() != null && cboSourceApplication.SelectedItem.ToString() == "Mattermost")
+            {
+                cboTypeofAudit.Items.Clear();
+                cboTypeofAudit.Text = string.Empty;
+                cboTypeofAudit.Items.Add("Chat Audit");
+                cboTypeofAudit.Enabled = true;
+                //cboTypeofAudit.Items.Add("Other Audit");
+            }
         }
 
         private void cboTypeofAudit_SelectedIndexChanged_1(object sender, EventArgs e)
         {
-
+            if (cboTypeofAudit != null)
+            {
+                btnGo.Enabled = true;
+                bttnChanges.Enabled = true;
+            }
         }
 
         private void btnGo_Click(object sender, EventArgs e)
         {
+            if (cboTypeofAudit.SelectedItem.ToString() != null && cboTypeofAudit.SelectedItem.ToString() == "Chat Audit")
+            {
+                frmMattermostChatsAudits frmMattermostChatsAudits = new frmMattermostChatsAudits(navUser);
+                frmMattermostChatsAudits.Show();
+                this.Close();
+            }
+            else
+            {
+                //nothing
+            }
 
+            if (cboTypeofAudit.SelectedItem.ToString() != null && cboTypeofAudit.SelectedItem.ToString() == "Email Audit")
+            {
+                frmEmailAuditReport frmEmailAuditReport = new frmEmailAuditReport(navUser);
+                frmEmailAuditReport.Show();
+                this.Close();
+            }
+            else
+            {
+                //nothing
+            }
         }
 
         private void bttnChanges_Click_1(object sender, EventArgs e)
         {
+            if (cboTypeofAudit.SelectedItem.ToString() != null && cboTypeofAudit.SelectedItem.ToString() == "Chat Audit")
+            {
+                frmMattermostChatCorrections frmMattermostChatCorrections = new frmMattermostChatCorrections(navUser);
+                frmMattermostChatCorrections.Show();
+                this.Close();
+            }
 
+            if (cboTypeofAudit.SelectedItem.ToString() != null && cboTypeofAudit.SelectedItem.ToString() == "Email Audit")
+            {
+                frmEmailAuditReport frmEmailAuditReport = new frmEmailAuditReport(navUser);
+                frmEmailAuditReport.Show();
+                this.Close();
+
+            }
         }
     }
 }
